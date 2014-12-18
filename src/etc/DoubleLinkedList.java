32e25954fb9a1484e1db95d46cd78eb97505bfe6
Maxmanski <maxmanski@live.at>
2014-12-18 15:44:54 +0100

    List Update

---
+++ src/etc/DoubleLinkedList.java
@@ -0,0 +1,20 @@
+package etc;
+
+public class DoubleLinkedList<A> {
+
+	public void add(A a){
+		
+	}
+	
+	public void remove(A a){
+		
+	}
+	
+	public void remove(int index){
+		
+	}
+	
+	public A get(int index){
+		return null;
+	}
+}
