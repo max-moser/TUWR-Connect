7cd47d01e169130c646764389bea56867b7afa3b
Maxmanski <maxmanski@live.at>
2014-12-18 15:52:08 +0100

    List Update

--- src/etc/DoubleLinkedList.java
+++ src/etc/DoubleLinkedList.java
@@ -1,12 +1,28 @@
 package etc;
 
+/**
+ * 
+ * @author Maxmanski
+ *
+ * @param <A>
+ */
 public class DoubleLinkedList<A> {
 
+	private ListNode root;
+	
 	public DoubleLinkedList(){
-		
+		this.root = null;
 	}
 	
 	public void add(A a){
+		this.addFirst(a);
+	}
+	
+	public void addFirst(A a){
+		
+	}
+	
+	public void addLast(A a){
 		
 	}
 	
@@ -36,5 +52,51 @@ public class DoubleLinkedList<A> {
 	
 	public A getLast(){
 		return null;
+	}
+	
+	public int size(){
+		if(this.root == null){
+			return 0;
+		}else{
+			return 0;
+		}
+	}
+	
+	public boolean isEmpty(){
+		return this.size() == 0;
+	}
+	
+	/**
+	 * 
+	 * @author Maxmanski
+	 *
+	 */
+	private class ListNode<A>{
+		private final A content;
+		private ListNode<A> succ, pred;
+		
+		public ListNode(A content, ListNode<A> successor, ListNode<A> predecessor){
+			this.content = content;
+			this.succ = successor;
+			this.pred = predecessor;
+		}
+		
+		public A getContent(){
+			return this.content;
+		}
+		public ListNode<A> getSuccessor(){
+			return this.succ;
+		}
+		public ListNode<A> getPredecessor(){
+			return this.pred;
+		}
+		
+		public void setPredecessor(ListNode<A> node){
+			this.pred = node;
+		}
+		
+		public void setSuccessor(ListNode<A> node){
+			this.succ = node;
+		}
 	}
 }
