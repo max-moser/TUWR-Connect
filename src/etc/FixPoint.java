4fd5b04630fcbb7bc04f167e44f98b61b1b50378
Max <maxmanski@live.at>
2015-02-06 22:45:28 +0100

    FixPoint Update

--- src/etc/FixPoint.java
+++ src/etc/FixPoint.java
@@ -11,28 +11,136 @@ public class FixPoint {
 	}
 
 	/**
+	 * TODO description
+	 * 
+	 * The following 
+	 * length > 0
+	 * length >= fixpoint
+	 * fixpoint >= 0 --OR-- fixpoint == -1, if disabled
 	 * 
 	 * @return
 	 */
 	public byte[] getFormatted(int fixpoint, int length){
-		byte[] ret = new byte[8]; // TODO
-		for(int i=0; i<ret.length; i++){
-			ret[i] = 0;
+		double dblVal, exp, tmpRes;
+		long preDec, postDec;
+		int preDecimalLength, postDecimalLength, len, bpb;
+		byte[] result;
+		boolean[] exps;
+		
+		boolean fixpointWork = (fixpoint >= 0);
+
+		bpb = Byte.SIZE; // bpb: bits per byte (should be 8)
+		len = ((length - 1) / bpb) + 1;
+		result = new byte[len];
+		for(int i=0; i<result.length; i++){
+			result[i] = 0;
 		}
+		
+		// initialisations in case that fixpoint == -1
+		preDecimalLength = length;
+		postDecimalLength = 0;
+		exps = new boolean[postDecimalLength]; // dummy init.
+		
+		if(fixpointWork){
+			preDecimalLength = fixpoint;
+			postDecimalLength = length - preDecimalLength;
 
-		// TODO
+			// inits
+			exps = new boolean[postDecimalLength];
+			for(int i=0; i<postDecimalLength; i++){
+				exps[i] = false;
+			}
 
-		ret[0] = number.byteValue();
+			tmpRes = 0;
+			dblVal = this.number.doubleValue();
+			dblVal = (dblVal - ((int)dblVal));
+			// dblVal now only contains the fractional part of the number
 
-		if(ret[0] > 0){
-			while(((~ret[0]) & 0b1000_0000) > 0){
-				ret[0] <<= 1;
+			// determine the needed exponents via first-fit heuristics
+			// the result should be:
+			// resulting value <= true value
+			// with the best approximation possible with the given amount of bits
+			for(int i=0; i<postDecimalLength; i++){
+				exp = Math.pow(2, ((-1) * (i + 1)));
+				if((exp + tmpRes) <= dblVal){
+					tmpRes += exp;
+					exps[i] = true;
+				}
 			}
 		}
 
-		ret[1] = 0;
+		// filling in the results
+		preDec = this.number.longValue();
+		postDec = 0;
 
-		return ret;
+		for(int i=0; i<postDecimalLength; i++){
+			postDec <<= 1;
+			if(exps[i]){
+				postDec = postDec | 0x1;
+			}
+		}
+
+		// creating the final result
+		// fixpointbyte: the byte in which the fixpoint is
+		// fixinbytepre: the amount of bits in the fixpointbyte before the fixpoint
+		// fixinbytepost: the amount of bits in the fixpointbyte after the fixpoint
+		// bytedist: the amount of bytes between the current byte and the fixpointbyte
+		int fixpointbyte, fixinbytepre, fixinbytepost, bytedist, shiftdist;
+		long tmp;
+
+		// trimbyte: byte used for trimming
+		byte trimbyte = 1;
+		for(int i=0; i<Byte.SIZE; i++){
+			trimbyte <<= 1;
+			trimbyte = (byte)(trimbyte | 1);
+		}
+
+
+		fixpointbyte = (len - 1);
+		fixinbytepre = bpb;
+		fixinbytepost = 0;
+
+		if(fixpointWork){
+			fixpointbyte = fixpoint / bpb;
+			fixinbytepre = fixpoint % bpb;
+			fixinbytepost = bpb - fixinbytepre;
+		}
+
+		// do the pre decimal stuff
+		for(int i=0; i<=fixpointbyte; i++){
+			bytedist = (fixpointbyte - i);
+			shiftdist = ((bytedist * bpb) - fixinbytepost);
+
+			if(shiftdist > 0){
+				tmp = (preDec >> shiftdist);
+			}else{
+				shiftdist = (-1) * shiftdist;
+				tmp = (preDec << shiftdist);
+			}
+
+			tmp = (tmp & trimbyte);
+			result[i] = (byte)(result[i] | ((byte)tmp));
+		}
+
+		if(fixpointWork){
+			// do the post decimal stuff
+			for(int i=fixpointbyte; i<len; i++){
+				bytedist = ((len - 1 ) - i);
+				shiftdist = (bytedist * bpb);
+
+				if(shiftdist > 0){
+					tmp = (postDec >> shiftdist);
+				}else{
+					shiftdist = (-1) * shiftdist;
+					tmp = (postDec << shiftdist);
+				}
+
+				tmp = (tmp & trimbyte);
+				result[i] = (byte)(result[i] | ((byte)tmp));
+			}
+		}
+
+		return result;
 	}
 
 	public String toString(){
--- src/main/Main.java
+++ src/main/Main.java
@@ -31,22 +31,23 @@ public class Main {
 			XMLParser xml = new XMLParser(handler);
 			xml.parse("command.xml");
 			List<Command> res = handler.getResult();
-			for(Command c: res){
-				System.out.println(c);
-			}
-			
-			Command c = res.get(0);
-			c.replaceParameter("modl", new FixPoint("1.0"));
-			c.replaceParameter("modr", new FixPoint("1.0"));
-			System.out.println(c.getParameter("modl"));
-			System.out.println(c.getParameter("modr"));
-			System.out.println(c.getParameter("right"));
-			byte[] data = CommandToCAN.getData(c);
-			
-			for(byte b: data){
-				System.out.print(Integer.toBinaryString(b) + "|");
-			}
-			System.out.println();
+			//			for(Command c: res){
+			//				System.out.println(c);
+			//			}
+			//			Command c = res.get(0);
+			//			c.replaceParameter("modl", new FixPoint("1.0"));
+			//			c.replaceParameter("modr", new FixPoint("1.0"));
+			//			System.out.println(c.getParameter("modl"));
+			//			System.out.println(c.getParameter("modr"));
+			//			System.out.println(c.getParameter("right"));
+			//			byte[] data = CommandToCAN.getData(c);
+			//			
+			//			for(byte b: data){
+			//				System.out.print(Integer.toBinaryString(b) + "|");
+			//			}
+			//			System.out.println();
+			FixPoint fp = new FixPoint("1.25");
+			System.out.println(bytesToString(fp.getFormatted(8, 16)));
 			
 		}catch(Exception e){
 			e.printStackTrace();
@@ -77,5 +78,35 @@ public class Main {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+
+	private static String bytesToString(byte[] bs){
+		String str = "";
+		for(byte b: bs){
+			str += byteToString(b) + " ";
+		}
+		return str;
+	}
+
+	private static String byteToString(byte b){
+		// trimbyte: byte used for trimming
+		byte trimbyte = 1;
+		for(int i=0; i<Byte.SIZE; i++){
+			trimbyte <<= 1;
+			trimbyte = (byte)(trimbyte | 1);
+		}
+		
+		int bi = b;
+		bi = bi & trimbyte;
+
+		String bStr = Integer.toBinaryString(bi);
+		String str = "";
+
+		for(int i=0; i<(Byte.SIZE - bStr.length()); i++){
+			str += "0";
+		}
+		str += bStr;
+
+		return str;
 	}
 }
